0347348232f775d311d93eddd830cfd3b8c62738
Scala Steward <scala_steward@virtuslab.com>
2024-12-03 22:26:16 +0000

    Update munit to 1.0.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -35,7 +35,7 @@ object Dependencies {
   )
 
   val mUnitTest: Seq[ModuleID] = Seq(
-    "org.scalameta" %% "munit"                   % "1.0.2",
+    "org.scalameta" %% "munit"                   % "1.0.3",
     "org.scalameta" %% "munit-scalacheck"        % "1.0.0",
     "org.typelevel" %% "munit-cats-effect"       % "2.0.0",
     "org.typelevel" %% "scalacheck-effect-munit" % "1.0.4"
